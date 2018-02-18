696044fa8f05a5a0aea1dd2e9e8dd19f5792e6b4
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2018-02-18 18:59:50 -0300

    change to version 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ldap-simple-search',
-      version='0.1',
+      version='0.1.1',
       description='Easily perform simple LDAP queries',
       url='http://github.com/chaordic/python-ldap-simple-search',
       author='Raphael P. Ribeiro',
