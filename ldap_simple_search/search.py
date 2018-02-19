e70d677b759e06aedbb46f90c42f2096b5144a58
Raphael P. Ribeiro <raphaelpr01@gmail.com>
2018-02-19 15:15:42 -0300

    avoid returns double list

--- ldap_simple_search/search.py
+++ ldap_simple_search/search.py
@@ -74,7 +74,7 @@ class LDAPSearch(object):
         for dn, record in data:
             if dn is not None:
                 if callback is not None:
-                    results.append((callback(dn, record)))
+                    results = callback(dn, record)
                 else:
                     results.append((dn, record))
 
